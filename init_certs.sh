8a63550a6a13ee91fd1ba5827252f83e1985d136
Robin Lee Powell <robinleepowell@gmail.com>
2021-08-12 22:19:56 -0700

    Added gdoc-to-ao3.digitalkingdom.org

--- init_certs.sh
+++ init_certs.sh
@@ -4,6 +4,7 @@ podman exec -u root -it web certbot certonly --standalone --expand \
     -d digitalkingdom.org -d davidleepowell.com -d teddyb.org \
     -d www.digitalkingdom.org -d www.davidleepowell.com -d www.teddyb.org \
     -d robinleepowell.name -d rlpowell.name \
+    -d gdoc-to-ao3.digitalkingdom.org \
     --non-interactive --agree-tos --email robinleepowell@gmail.com --http-01-port=8888
 podman exec -u root -it web chown -R $(id -un):$(id -gn) /etc/letsencrypt/
 
