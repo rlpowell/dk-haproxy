08d3e8b219a5569e19e499622365e29a8c438842
Robin Lee Powell <robinleepowell@gmail.com>
2021-08-12 22:12:20 -0700

    More with the actually working.

--- renew_certs.sh
+++ renew_certs.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# This is often called from cron, so:
+export PATH=$HOME/.local/bin:$HOME/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:$PATH
+
 podman exec -u root -it web certbot renew --tls-sni-01-port=8888
 podman exec -u root -it web chown -R $(id -un):$(id -gn) /etc/letsencrypt/
 
-cat containers/web/data/letsencrypt/live/digitalkingdom.org/fullchain.pem containers/web/data/letsencrypt/live/digitalkingdom.org/privkey.pem > containers/web/data/letsencrypt/live/digitalkingdom.org/haproxy.pem
+cat /home/spdkhaproxy/dk-haproxy/containers/web/data/letsencrypt/live/digitalkingdom.org/fullchain.pem \
+    /home/spdkhaproxy/dk-haproxy/containers/web/data/letsencrypt/live/digitalkingdom.org/privkey.pem > \
+    /home/spdkhaproxy/dk-haproxy/containers/web/data/letsencrypt/live/digitalkingdom.org/haproxy.pem
 
 systemctl --user restart web
