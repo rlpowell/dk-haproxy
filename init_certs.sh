54900a5afd0a604279205cf7e5a04e4f8a12d16e
Robin Lee Powell <robinleepowell@gmail.com>
2026-04-21 13:43:53 -0700

    Added hashbrowns, lemonade, and unifi

--- init_certs.sh
+++ init_certs.sh
@@ -6,6 +6,8 @@ podman exec -u root -it web certbot certonly --standalone --expand \
     -d robinleepowell.name -d rlpowell.name \
     -d gdoc-to-ao3.digitalkingdom.org \
     -d waffles.digitalkingdom.org \
+    -d hashbrowns.digitalkingdom.org \
+    -d lemonade.digitalkingdom.org \
     -d users.digitalkingdom.org \
     -d mail.digitalkingdom.org -d stodi.digitalkingdom.org \
     -d mail.teddyb.org -d stodi.teddyb.org \
@@ -20,4 +22,4 @@ systemctl --user restart web
 # .realm certs
 CAROOT="$HOME/dk-haproxy/misc/realm_certs/ca_root/" TRUST_STORES='none' mkcert -install
 # Don't try to do a *.realm wildcard; it seems to not work in chrome
-CAROOT="$HOME/dk-haproxy/misc/realm_certs//ca_root/" TRUST_STORES='none' mkcert -cert-file misc/realm_certs/star.realm.pem -key-file misc/realm_certs/star.realm.key -p12-file misc/realm_certs/star.realm.p12 hassio.realm waffles.realm plex.realm vaultwarden.realm pihole.realm pi-hole.realm hassio.r3alm waffles.r3alm plex.r3alm vaultwarden.r3alm pihole.r3alm pi-hole.r3alm 192.168.123.127
+CAROOT="$HOME/dk-haproxy/misc/realm_certs//ca_root/" TRUST_STORES='none' mkcert -cert-file misc/realm_certs/star.realm.pem -key-file misc/realm_certs/star.realm.key -p12-file misc/realm_certs/star.realm.p12 hassio.realm plex.realm vaultwarden.realm pihole.realm pi-hole.realm hassio.r3alm waffles.realm waffles.r3alm hashbrowns.realm hashbrowns.r3alm lemonade.realm lemonade.r3alm plex.r3alm vaultwarden.r3alm pihole.r3alm pi-hole.r3alm unifi.realm unifi.r3alm 192.168.123.127
